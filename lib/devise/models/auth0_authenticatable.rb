ce6d3c03c3f35529bfcaac4364d82f4b4c098de0
Derek Kastner <dkastner@gmail.com>
2017-08-23 15:50:41 -0500

    Fix error reporting

--- lib/devise/models/auth0_authenticatable.rb
+++ lib/devise/models/auth0_authenticatable.rb
@@ -14,7 +14,7 @@ module Devise
         def find_or_sync_auth0(info)
           unless uid = info['sub'] || info['user_id']
             raise MissingAuth0Id.new(info),
-              "Expected auth0_user_id, got none inside of #{@info.inspect}"
+              "Expected auth0_user_id, got none inside of #{info.inspect}"
           end
 
           if user = User.find_by(auth0_user_id: uid)
